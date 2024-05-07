c47a095a666b408b21091b91b5125ff537de02d8
Jiseon Min <minjiseon0827@gmail.com>
2024-05-07 16:50:28 -0700

    typo for importing ts_simulators fixed, use snakemake.params

--- workflow/scripts/train_npe.py
+++ workflow/scripts/train_npe.py
@@ -3,16 +3,14 @@ import os
 import pickle
 
 from embedding_networks import ExchangeableCNN
-from simulators import AraTha_2epoch_simulator
-import defopt
+from ts_simulators import AraTha_2epoch_simulator
 import numpy as np
 import torch
 from sbi.inference import SNPE
 from sbi.inference.posteriors import DirectPosterior
 from sbi.utils import posterior_nn
 
-
-def load_data_files(data_dir: str):
+def load_data_files(data_dir):
     """
     Function that loads the simulated data from a given directory, converts to
     torch and reshapes as necessary
@@ -35,42 +33,37 @@ def load_data_files(data_dir: str):
     return thetas, xs
 
 
-def main(
-    data_dir: str,
-    outdir: str,
-):
+data_dir = snakemake.params.datadir
+outdir = snakemake.params.outdir
 
-    thetas, xs = load_data_files(data_dir)
-    simulator = AraTha_2epoch_simulator()
-    prior = simulator.prior
-
-    embedding_net = ExchangeableCNN().cuda()
-    normalizing_flow_density_estimator = posterior_nn(
-        model="maf_rqs",
-        z_score_x="none",
-        embedding_net=embedding_net,
-        hidden_features=64,
-        num_transforms=6,
-    )
-    inference = SNPE(
-        prior,
-        density_estimator=normalizing_flow_density_estimator,
-        device="cuda",
-        show_progress_bars=True,
-    )
-    posterior_estimator = inference.append_simulations(thetas, xs).train(
-        show_train_summary=True,
-        retrain_from_scratch=True,
-        validation_fraction=0.2,
-    )
-    posterior = DirectPosterior(posterior_estimator, prior, device="cuda")
+thetas, xs = load_data_files(data_dir)
+simulator = AraTha_2epoch_simulator()
+prior = simulator.prior
+# Todo : make the embedding network customizable
+embedding_net = ExchangeableCNN().cuda()
+normalizing_flow_density_estimator = posterior_nn(
+    model="maf_rqs",
+    z_score_x="none",
+    embedding_net=embedding_net,
+    hidden_features=64,
+    num_transforms=6,
+)
+inference = SNPE(
+    prior,
+    density_estimator=normalizing_flow_density_estimator,
+    device="cuda",
+    show_progress_bars=True,
+)
+posterior_estimator = inference.append_simulations(thetas, xs).train(
+    show_train_summary=True,
+    retrain_from_scratch=True,
+    validation_fraction=0.2,
+)
+posterior = DirectPosterior(posterior_estimator, prior, device="cuda")
 
 
-    if not os.path.isdir(outdir):
-        os.mkdir(outdir)
-    pkl_file = f"{outdir}/posterior.pkl"
-    with open(pkl_file, "wb") as f:
-        pickle.dump(posterior, f)
-
-if __name__ == "__main__":
-    defopt.run(main)
+if not os.path.isdir(outdir):
+    os.mkdir(outdir)
+pkl_file = f"{outdir}/posterior.pkl"
+with open(pkl_file, "wb") as f:
+    pickle.dump(posterior, f)
