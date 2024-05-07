e6b89c25c311695edd8a28c156d24cbe225f5417
Jiseon Min <minjiseon0827@gmail.com>
2024-05-07 16:49:38 -0700

    use snakemake.params

--- workflow/scripts/process_ts.py
+++ workflow/scripts/process_ts.py
@@ -1,24 +1,23 @@
 import tskit
 import os
-import defopt
 from ts_processors import *
 import torch
 from ts_simulators import *
 import numpy as np
 
-def main(num_simulations: int, outdir: str):
-    with open(f"{outdir}/{num_simulations}.trees", "rb") as ts_file:
-        ts = tskit.load(ts_file)
+outdir = snakemake.params.outdir
+num_simulations = snakemake.params.num_simulations
+n_snps = snakemake.params.n_snps
 
-    # Todo : config parameter that chooses dinf_extract as an option and an if statement (arg == dinf_extract)
+with open(f"{outdir}/{num_simulations}.trees", "rb") as ts_file:
+    ts = tskit.load(ts_file)
 
-    simulator = AraTha_2epoch_simulator()
-    n_sample = simulator.n_sample
-    processor = dinf_extract(n_snps=2000)
-    x = processor(ts)
-    # x is tensor, so change it to numpy first and save it as .npy
-    x = x.squeeze().cpu().numpy()
-    np.save(f"{outdir}/x_{num_simulations}.npy",x)
- 
-if __name__ == "__main__":
-    defopt.run(main)
+
+simulator = AraTha_2epoch_simulator()
+n_sample = simulator.n_sample
+# Todo : make the processor customizable
+processor = dinf_extract(n_snps=n_snps)
+x = processor(ts)
+# x is tensor, so change it to numpy first and save it as .npy
+x = x.squeeze().cpu().numpy()
+np.save(f"{outdir}/x_{num_simulations}.npy",x)
