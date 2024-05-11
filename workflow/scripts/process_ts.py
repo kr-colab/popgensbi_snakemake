ec972864aab1dec0c091fa3da313d107baffe518
Jiseon Min <minjiseon0827@gmail.com>
2024-05-10 18:42:54 -0700

    dinf is chosen as an option from config, params for dinf is handled in ts_processors.py

--- workflow/scripts/process_ts.py
+++ workflow/scripts/process_ts.py
@@ -12,11 +12,9 @@ n_snps = snakemake.params.n_snps
 with open(f"{outdir}/{num_simulations}.trees", "rb") as ts_file:
     ts = tskit.load(ts_file)
 
+if snakemake.params.ts_processor == "dinf":
+    processor = dinf_extract(snakemake)
 
-simulator = AraTha_2epoch_simulator()
-n_sample = simulator.n_sample
-# Todo : make the processor customizable
-processor = dinf_extract(n_snps=n_snps)
 x = processor(ts)
 # x is tensor, so change it to numpy first and save it as .npy
 x = x.squeeze().cpu().numpy()
