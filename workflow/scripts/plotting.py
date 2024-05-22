b8fe1c936f29f6f2513e61b7c4438f543f8984b6
Jiseon Min <minjiseon0827@gmail.com>
2024-05-22 11:49:44 -0700

    use 100,000 points to visualize posterior

--- workflow/scripts/plotting.py
+++ workflow/scripts/plotting.py
@@ -24,7 +24,7 @@ try:
     x_obs = np.load(os.path.join(datadir, "x_obs.npy"))
     # Reshape x_obs to (1, *x_obs.shape) so that it knows that batch size is 1
     x_obs = torch.from_numpy(x_obs.reshape(1, *x_obs.shape))
-    samples = posterior.sample((1_000_000,), x=x_obs.cuda(), show_progress_bars=True).cpu().numpy()
+    samples = posterior.sample((100_000,), x=x_obs.cuda(), show_progress_bars=True).cpu().numpy()
 
     np.save(f"{posteriordir}/round_{rounds}/default_obs_samples.npy", samples)
     _ = corner.corner(
