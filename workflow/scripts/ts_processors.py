ecf6fca77982179de83002b00ff7580e09de2ef8
Jiseon Min <minjiseon0827@gmail.com>
2024-05-23 12:56:26 -0700

    added jsfs for two-pops model, fixed dinf multiple pops

--- workflow/scripts/ts_processors.py
+++ workflow/scripts/ts_processors.py
@@ -61,27 +61,31 @@ class dinf_extract_multiple_pops(BaseProcessor):
         from dinf.misc import ts_individuals
         # recover population names and corresponding sampled individuals
         pop_names = [pop.metadata['name'] for pop in ts.populations()]
-        individuals = {name: ts_individuals(ts, name) for name in pop_names}
-        num_individuals = {name: len(individuals[name]) for name in pop_names}
+        # make a dictionary with sampled individuals for each population except for unsampled ones
+        sampled_pop_names = [pop for pop in pop_names if len(ts_individuals(ts, pop)) > 0]
+        individuals = {name: ts_individuals(ts, name) for name in sampled_pop_names}
+        num_individuals = {name: len(individuals[name]) for name in sampled_pop_names}
 
             
         extractor = dinf.feature_extractor.MultipleHaplotypeMatrices(
             num_individuals=num_individuals, 
-            num_loci=self.n_snps,
-            ploidy=self.ploidy,
-            global_phased={pop: self.phased for pop in pop_names},
-            global_maf_thresh={pop: self.maf_thresh for pop in pop_names}
+            num_loci={pop: self.n_snps for pop in sampled_pop_names},
+            ploidy={pop: self.ploidy for pop in sampled_pop_names},
+            global_phased=self.phased,
+            global_maf_thresh=self.maf_thresh
         )
         # we get a dictionary of feature matrices, one for each population
         feature_matrices = extractor.from_ts(ts, individuals=individuals)
         # Each feature matrix has dimension of (num_individual, num_loci, 2). 
         # because num_individual can be different for each population, we need to pad them with -1
-        max_num_individuals = max([num_individuals[pop] for pop in pop_names])
-        for pop in pop_names:
+        max_num_individuals = max([num_individuals[pop] for pop in sampled_pop_names])
+
+        for pop in individuals.keys():
+            feature_matrices[pop] = torch.from_numpy(feature_matrices[pop])
             num_individuals = feature_matrices[pop].shape[0]
             if num_individuals < max_num_individuals:
-                feature_matrices[pop] = np.concatenate([feature_matrices[pop], -np.ones((max_num_individuals - num_individuals, self.n_snps, 2))])
-        output_mat = torch.stack([v for v in torch.from_numpy(feature_matrices.values())]).permute(0, 3, 1, 2)
+                feature_matrices[pop] = torch.nn.functional.pad(feature_matrices[pop], (0, 0, 0, 0, 0, max_num_individuals - num_individuals), "constant", -1)
+        output_mat = torch.stack([v for v in feature_matrices.values()]).permute(0, 3, 1, 2)
         return output_mat
 
 
@@ -171,6 +175,30 @@ class tskit_sfs(BaseProcessor):
             span_normalise = self.span_normalise, 
             polarised = self.polarised)
         return torch.from_numpy(sfs).float()
+
+class tskit_jsfs(BaseProcessor):
+    '''
+    Joint site frequency spectrum for two-population model
+    '''
+    params_default = {
+        "windows": None,
+        "mode": "site",
+        "span_normalise": True,
+        "polarised": False
+    }
+    def __init__(self, snakemake):
+        super().__init__(snakemake, tskit_sfs.params_default)
+    def __call__(self, ts):
+        sfs = ts.allele_frequency_spectrum(
+            sample_sets = [ts.samples(population=0), ts.samples(population=1)], 
+            windows = self.windows, 
+            mode = self.mode, 
+            span_normalise = self.span_normalise, 
+            polarised = self.polarised)
+        return torch.from_numpy(sfs).float()
+
+
+
 
 class tskit_sfs_selection(BaseProcessor):
     ## get SFS with synnonymous and non-synonymous mutations separately and append the two arrays to get a single array
