9230dc49128407aa7798c18645d32bbe430d37c9
Jiseon Min <minjiseon0827@gmail.com>
2024-06-12 17:14:54 -0700

    use default CNN embedding (is bigger than before)

--- workflow_dadi/scripts/train_npe.py
+++ workflow_dadi/scripts/train_npe.py
@@ -40,36 +40,52 @@ simulator = MODEL_LIST[snakemake.params.demog_model](snakemake)
 prior = simulator.prior
 ns = simulator.ns
 
+# using default setup of CNN embedding network in sbi
 embedding_net = CNNEmbedding(
     input_shape=(ns[0]+1, ns[1]+1),
-    in_channels=1,
-    out_channels_per_layer=[6],
-    num_conv_layers=1,
-    num_linear_layers=1,
-    output_dim=8,
-    kernel_size=5,
-    pool_kernel_size=8
 )
+# embedding_net = CNNEmbedding(
+#     input_shape=(ns[0]+1, ns[1]+1),
+#     in_channels=1,
+#     out_channels_per_layer=[6],
+#     num_conv_layers=1,
+#     num_linear_layers=1,
+#     output_dim=8,
+#     kernel_size=5,
+#     pool_kernel_size=8
+# )
 
 normalizing_flow_density_estimator = posterior_nn(
     model="maf",
     embedding_net=embedding_net
 )
 
-log_dir = os.path.join(posteriordir, f"sim_round_{sim_rounds}", "sbi_logs")
+log_dir = os.path.join(posteriordir, "sbi_logs", f"sim_round_{sim_rounds}")
 writer = SummaryWriter(log_dir=log_dir)
-if int(sim_rounds) == 0:
-    inference = SNPE(
+# if int(sim_rounds) == 0:
+#     inference = SNPE(
+#     prior=prior, 
+#     density_estimator=normalizing_flow_density_estimator, 
+#     device="cuda",
+#     show_progress_bars=True, 
+#     summary_writer=writer)
+# else:
+#     with open(os.path.join(posteriordir, f"sim_round_{int(sim_rounds)-1}", f"inference.pkl"), "rb") as f:
+#         inference = pickle.load(f)
+
+# inference = inference.append_simulations(thetas, xs, proposal=prior)
+
+inference = SNPE(
     prior=prior, 
     density_estimator=normalizing_flow_density_estimator, 
     device="cuda",
     show_progress_bars=True, 
     summary_writer=writer)
-else:
-    with open(os.path.join(posteriordir, f"sim_round_{int(sim_rounds)-1}", f"inference.pkl"), "rb") as f:
-        inference = pickle.load(f)
 
-inference = inference.append_simulations(thetas, xs, proposal=prior)
+for n in range(int(sim_rounds)+1):
+    thetas, xs = load_data_files(datadir, n)
+    inference = inference.append_simulations(thetas, xs, proposal=prior)
+
 posterior_estimator = inference.train(show_train_summary=True, 
     force_first_round_loss=True, 
     validation_fraction=0.2)
