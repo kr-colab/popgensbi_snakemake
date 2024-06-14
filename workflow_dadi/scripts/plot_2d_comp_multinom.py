9c302a6f38d18aa8f19700eaa911b6dd8c46fa71
Jiseon Min <minjiseon0827@gmail.com>
2024-06-14 16:56:49 -0700

    lowering threshold for plotting

--- workflow_dadi/scripts/plot_2d_comp_multinom.py
+++ workflow_dadi/scripts/plot_2d_comp_multinom.py
@@ -23,6 +23,6 @@ model_fs = np.load(os.path.join(posteriordir, f"sim_round_{sim_rounds}/model_fs.
 
 import pylab
 pylab.figure(figsize=(8,6))
-dadi.Plotting.plot_2d_comp_multinom(dadi.Spectrum(model_fs), dadi.Spectrum(data), vmin=1e-4)
+dadi.Plotting.plot_2d_comp_multinom(dadi.Spectrum(model_fs), dadi.Spectrum(data), vmin=1e-5)
 pylab.savefig(os.path.join(posteriordir, f"sim_round_{sim_rounds}/2d_comp_multinom.png"))
 
