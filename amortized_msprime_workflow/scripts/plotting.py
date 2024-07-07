6ed9c0cade6db2c6eb62c4f9ad0edb3ce98f146e
Jiseon Min <minjiseon0827@gmail.com>
2024-07-06 19:25:08 -0700

    load tskit

--- amortized_msprime_workflow/scripts/plotting.py
+++ amortized_msprime_workflow/scripts/plotting.py
@@ -22,7 +22,7 @@ with open(f"{posteriordir}/{ts_processor}/sim_round_{sim_rounds}/ensemble_poster
     posterior = pickle.load(f)   
 
 try:
-    x_obs = np.load(os.path.join(datadir, ts_processor "x_obs.npy"))
+    x_obs = np.load(os.path.join(datadir, ts_processor, "x_obs.npy"))
     # Reshape x_obs to (1, *x_obs.shape) so that it knows that batch size is 1
     x_obs = torch.from_numpy(x_obs.reshape(1, *x_obs.shape))
     samples = posterior.sample((100_000,), x=x_obs.cuda(), show_progress_bars=True).cpu().numpy()
