d8d3c07ad9b2db48230b8f089b35e40b3307fe48
Jiseon Min <minjiseon0827@gmail.com>
2024-09-20 08:37:36 -0700

    save confidence interval in posterior folder, no need for max_n_train in this script

--- amortized_dadi_workflow/scripts/plot_confidence_intervals.py
+++ amortized_dadi_workflow/scripts/plot_confidence_intervals.py
@@ -7,7 +7,6 @@ from dadi_simulators import *
 datadir = snakemake.params.datadir
 posteriordir = snakemake.params.posteriordir
 n_trains = snakemake.params.n_trains
-max_n_train = snakemake.params.max_n_train
 for j, n_train in enumerate(n_trains):
     posterior_samples = np.load(os.path.join(posteriordir, f"n_train_{int(float(n_train))}", "default_obs_samples.npy"))
     widths = []
