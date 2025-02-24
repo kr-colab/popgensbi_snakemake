bae40d94082c1e197e7b6001b68b303e0ff16b50
Andrew Kern <adkern@uoregon.edu>
2025-02-24 13:58:40 -0800

    changed handling of metadata to csv file

--- workflow/scripts/add_populations_metadata.py
+++ workflow/scripts/add_populations_metadata.py
@@ -6,21 +6,26 @@ import numpy as np
 
 ds = zarr.load(snakemake.input.zarr)
 
-populations = snakemake.params.populations
-pop_names = list(populations.keys())
+populations_csv_path = snakemake.params.popn_csv_path
+populations_array = np.genfromtxt(populations_csv_path, delimiter=",", dtype=str, skip_header=1)
+pop_dict = {row[0]: row[1] for row in populations_array}
+pop_names = list(pop_dict.values())
+
 # save the population data in json format
 schema = json.dumps(tskit.MetadataSchema.permissive_json().schema).encode()
 zarr.save(snakemake.input.zarr + "/populations_metadata_schema", schema)
 metadata = [
-    json.dumps({"name": pop, "description": "The country from which this sample comes"}).encode()
-    for pop in populations.keys()
+    json.dumps({"name": pop, "description": "population this sample comes from"}).encode()
+    for pop in pop_names
 ]
 zarr.save(snakemake.input.zarr + "/populations_metadata", metadata)
 
 # Now assign each diploid sample to a population
 num_individuals = ds["sample_id"].shape[0]
 individuals_population = np.full(num_individuals, tskit.NULL, dtype=np.int32)
-for pop, pop_size in populations.items():
-    for i in range(pop_size):
-        individuals_population[i] = pop_names.index(pop)
+# loop through each row in the populations array
+for i, sample_id in enumerate(ds["sample_id"]):
+    pop_id = pop_dict[sample_id]
+    individuals_population[i] = pop_names.index(pop_id)
+
 zarr.save(snakemake.input.zarr + "/individuals_population", individuals_population)
